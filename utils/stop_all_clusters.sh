0496e04b5178bb13368c1e2c5aeff6925a2139bd
levi106 <16788901+levi106@users.noreply.github.com>
2023-02-18 15:41:25 +0900

    add some actions

---
+++ utils/stop_all_clusters.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+Q='Resources
+| where type =~ "Microsoft.ContainerService/managedClusters"
+| where resourceGroup startswith "aksWorkshopRG-"'
+
+az config set extension.use_dynamic_install=yes_without_prompt
+az graph query -q "$Q" --query "data[].[name,resourceGroup]" -o tsv \
+| xargs -n 2 sh -c 'az aks stop -n $0 -g $1'
