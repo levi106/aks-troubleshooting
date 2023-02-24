158d78acd834a9b74006a35ce0defb7d7a1f741d
levi106 <16788901+levi106@users.noreply.github.com>
2023-02-24 17:23:13 +0900

    webtests actions

---
+++ utils/disable_all_webtests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Q='Resources
+| where type =~ "Microsoft.Insights/webtests"
+| where resourceGroup startswith "aksWorkshopRG-"'
+
+az graph query -q "$Q" --query "data[].[name,resourceGroup]" -o tsv \
+| xargs -n 2 sh -c 'az monitor app-insights web-test update -n $0 -g $1 --enabled false --no-wait'
