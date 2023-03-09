3bcffc3023363031b3e87183fd70274dee12e212
levi106 <16788901+levi106@users.noreply.github.com>
2023-03-09 21:28:31 +0900

    add day4 scripts

---
+++ utils/delete_day4_pre.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+Q='Resources
+| where type =~ "Microsoft.ContainerService/managedClusters"
+| where resourceGroup startswith "aksWorkshopRG-"'
+
+az graph query -q "$Q" --query "data[].[name,resourceGroup]" -o tsv \
+| xargs -n 2 sh -c 'az aks get-credentials -n $0 -g $1 \
+&& kubectl delete -k k8s/nginx/overlays/day4'
---
+++ utils/deploy_day4.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+  echo ./deploy_day4.sh "SQL Admin password"
+  exit 1
+fi
+
+Q='Resources
+| where type =~ "Microsoft.ContainerService/managedClusters"
+| where resourceGroup startswith "aksWorkshopRG-"'
+
+serverName=$(az sql server list -g aksWorkshopRG --query "[].name" --output tsv)
+dbName=$(az sql db list -g aksWorkshopRG -s ${serverName} --query "[].name" --output tsv | grep "db-")
+echo "url=jdbc:sqlserver://${serverName}.database.windows.net:1433;database=${dbName};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net" > k8s/web_app/overlays/day4/.env.db
+echo "username=sqladmin@${serverName}" >> k8s/web_app/overlays/day4/.env.db
+echo "password=${1}" >> k8s/web_app/overlays/day4/.env.db
+
+az graph query -q "$Q" --query "data[].[name,resourceGroup]" -o tsv \
+| xargs -n 2 sh -c 'echo connectionstring=$(az monitor app-insights component show -g $1 --query "[0].connectionString" -o tsv) > k8s/web_app/overlays/day4/.env.ai \
+&& az aks get-credentials -n $0 -g $1 \
+&& kubectl apply -k k8s/web_app/overlays/day4'
