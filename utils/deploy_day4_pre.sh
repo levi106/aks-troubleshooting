172ecefbddf855f22502df9d5ea58ebc7d075c77
levi106 <16788901+levi106@users.noreply.github.com>
2023-03-09 18:25:19 +0900

    fix path

--- utils/deploy_day4_pre.sh
+++ utils/deploy_day4_pre.sh
@@ -6,4 +6,4 @@ Q='Resources
 
 az graph query -q "$Q" --query "data[].[name,resourceGroup]" -o tsv \
 | xargs -n 2 sh -c 'az aks get-credentials -n $0 -g $1 \
-&& kubectl apply -k k8s/web_app/overlays/day4pre'
+&& kubectl apply -k k8s/nginx/overlays/day4'
