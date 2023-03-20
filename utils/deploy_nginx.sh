747bc6e3cffa737d6a3560e386699b2511c00917
levi106 <16788901+levi106@users.noreply.github.com>
2023-03-20 11:11:53 +0900

    rename

--- utils/deploy_nginx.sh
+++ utils/deploy_nginx.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]; then
+  echo ./deploy_nginx.sh "overlay path"
+  exit 1
+fi
+
+export layer=$1
+
 Q='Resources
 | where type =~ "Microsoft.ContainerService/managedClusters"
 | where resourceGroup startswith "aksWorkshopRG-"'
 
 az graph query -q "$Q" --query "data[].[name,resourceGroup]" -o tsv \
 | xargs -n 2 sh -c 'az aks get-credentials -n $0 -g $1 \
-&& kubectl apply -k k8s/nginx/overlays/day4'
+&& kubectl apply -k k8s/nginx/overlays/${layer}'
